181ca10af8ea574073e1d5014eb3e84209600206
Dariusz Ruchała <darek40bc@gmail.com>
2017-10-30 23:59:27 +0100

    init

---
+++ src/main/java/com/example/app/homepage/HomepageController.java
@@ -0,0 +1,20 @@
+package com.example.app.homepage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+
+	@RequestMapping("/")
+	public String showHomepage(Model model) {
+		model.addAttribute("title", "nowy tytuł");
+		return "homepage/homepage";
+	}
+	
+	@RequestMapping("sign_up")
+	public String showSignUp() {
+		return "homepage/sign_up";
+	}
+}
