5bc3681374b07b3796ab3985f20df5adaa3abf29
Dariusz R <darek40bc@gmail.com>
2017-11-24 19:26:08 +0100

    #1281 Created required tables

---
+++ Sources/sql/create_tables.sql
@@ -0,0 +1,49 @@
+create table users (
+	id serial primary key,
+	login varchar(100) not null,
+	password varchar(100) not null,
+	permissions integer not null,
+	name varchar(100),
+	surname varchar(100)
+);
+
+create table groups(
+	id serial primary key,
+	name varchar(100) not null,
+	description varchar(4000)
+);
+
+create table user_group(
+	id serial primary key,
+	user_id integer not null,
+	group_id integer not null
+);
+
+create table made_reservations(
+	id serial primary key,
+	reservation_id integer not null,
+	user_id integer,
+	resource_id integer not null
+);
+
+create table resource_groups(
+	id serial primary key,
+	name varchar(100) not null,
+	description varchar(4000)
+);
+
+create table resources(
+	id serial primary key,
+	resource_group_id integer not null,
+	name varchar(100) not null
+);
+
+create table reservations(
+	id serial primary key,
+	name varchar(100) not null,
+	description varchar(4000),
+	group_id integer not null,
+	author_id integer not null
+);
+
+
--- Sources/sql/create_user_table.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-	id serial PRIMARY KEY,
-	login varchar(100) not null,
-	password varchar(100) not null,
-	name varchar(100),
-	surname varchar(100)
-);
